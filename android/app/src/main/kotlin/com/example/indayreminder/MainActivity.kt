1faf1054d416c426480bb53c0848979a4f9b7ad6
Manu <manu@gmail.com>
2025-08-10 21:01:16 +0530

    initial commit

---
+++ android/app/src/main/kotlin/com/example/indayreminder/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.indayreminder
+
+
+import android.app.AlarmManager
+import android.content.Context
+import android.content.Intent
+import android.os.Build
+import android.provider.Settings
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity() {
+    override fun onResume() {
+        super.onResume()
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            val alarmManager = getSystemService(Context.ALARM_SERVICE) as AlarmManager
+            if (!alarmManager.canScheduleExactAlarms()) {
+                val intent = Intent(Settings.ACTION_REQUEST_SCHEDULE_EXACT_ALARM)
+                startActivity(intent)
+            }
+        }
+    }
+}
